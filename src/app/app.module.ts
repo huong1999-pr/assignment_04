6ed547113ce8c3ee4a377edb35d7af165211587a
huong1999 <huongyaya@gmail.com>
2022-07-28 23:22:02 +0700

    formatcode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ChildComponent } from './child/child.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
-//import { LoginComponent } from './login/login.component'
-import { MatIconModule } from '@angular/material/icon';
-//import { Exercise1Component } from './exercise1/exercise1.component';
-import { HttpClientModule } from '@angular/common/http';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
- 
- 
- 
- 
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ChildComponent } from "./child/child.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatIconModule } from "@angular/material/icon";
+import { HttpClientModule } from "@angular/common/http";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatSortModule } from "@angular/material/sort";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { FormsModule } from "@angular/forms";
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    ChildComponent,
- 
-  ],
+  declarations: [AppComponent, ChildComponent],
   imports: [
     BrowserModule,
     FormsModule,
@@ -42,9 +33,8 @@ import { FormsModule } from '@angular/forms';
     MatPaginatorModule,
     MatFormFieldModule,
     HttpClientModule,
-
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,49 +1,39 @@
-//import { Component, OnInit } from '@angular/core';
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import {Subject} from 'rxjs';
- 
-import { FormGroup, FormBuilder, Validators } from '@angular/forms'
-import { Router } from '@angular/router';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { Subject } from "rxjs";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 @Component({
-  selector: 'app-child',
-  templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css']
+  selector: "app-child",
+  templateUrl: "./child.component.html",
+  styleUrls: ["./child.component.css"],
 })
 export class ChildComponent implements OnInit {
-
   hide: boolean = false;
-  @Input('parentText') parentTextBoxValue: String;
+  @Input("parentText") parentTextBoxValue: String;
   @Input() parentClick: Subject<void>;
-  @Output('ontextFromChild') passToParent: EventEmitter<String> = new EventEmitter<String>();
-
+  @Output("ontextFromChild") passToParent: EventEmitter<String> =
+    new EventEmitter<String>();
   clickCount = 0;
-  email:String;
-  constructor(private fb: FormBuilder, private router: Router) { }
-
+  email: String;
+  constructor(private fb: FormBuilder, private router: Router) {}
   ngOnInit() {
     this.parentClick.subscribe(() => this.incrementValue());
   }
-
   incrementValue() {
     this.clickCount = this.clickCount + 1;
   }
   loginForm: FormGroup = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]]
-  })
+    email: ["", [Validators.required, Validators.email]],
+    password: ["", [Validators.required, Validators.minLength(6)]],
+  });
   onLogin() {
     if (!this.loginForm.valid) {
       return;
-     // this.passToParent.emit(this.email);
     }
     console.log(this.loginForm.value);
     this.passToParent.emit(this.email);
-     
   }
   onOpenPage() {
-    // this.router.navigate(['/exercise1'])
-    // console.log("this.email, this.email");
-    this.router.navigateByUrl('/exercise1');
+    this.router.navigateByUrl("/exercise1");
   }
-
 }
