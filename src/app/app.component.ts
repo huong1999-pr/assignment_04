8676c71b29ceddd409df3f0c9cddee233619fbb6
huong1999 <huongyaya@gmail.com>
2022-07-28 23:48:16 +0700

    format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,20 @@
-import { Component } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component } from "@angular/core";
+import { Subject } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'bt4';
+  title = "bt4";
   parentTxtValue: String;
-  parentClick:Subject<void> = new Subject<void>();
+  parentClick: Subject<void> = new Subject<void>();
   email: String;
-
   onParentButtonClick() {
     this.parentClick.next();
   }
-
   updateInChildValue(event) {
     this.email = event;
-
   }
 }
